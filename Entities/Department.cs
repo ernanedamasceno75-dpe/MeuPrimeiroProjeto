0b47840cc8938cab213a7b966149d88b4d257cfe
Ernane Damasceno <ernanedamasceno75@gmail.com>
2025-11-17 10:21:08 +0000

    Primeiro commit - projeto inicial

---
+++ Entities/Department.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace PrimeiroProjetoVs.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/HourContract.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace PrimeiroProjetoVs.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+
+    }
+}
